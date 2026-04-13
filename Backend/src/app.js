413ccfae105f95ab8aaae9bd1a6681cc054841ae
mohittiwario3 <mttiwari301@gmail.com>
2026-04-13 17:09:36 +0530

    feat: initialize express application with middleware and route configurations

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,6 +15,9 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
+app.get("/", (req, res) => {
+    res.send("server is running...")
+})
 
 /* using all the routes here */
 app.use("/api/auth", authRouter)
